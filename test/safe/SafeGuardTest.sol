9aa2b15385ec5c8c29f735773ee1ef7cc7de9409
Razorback <raz.w@binance.com>
2025-02-26 11:26:51 +0800

    test: use BSC_RPC env for fork testing

--- test/safe/SafeGuardTest.sol
+++ test/safe/SafeGuardTest.sol
@@ -16,7 +16,7 @@ contract SafeGuardTest is Test {
 
   function setUp() public {
     // fork mainnet
-    vm.createSelectFork("https://bsc.nodereal.io", 43797673);
+    vm.createSelectFork(vm.envString("BSC_RPC"), 43797673);
 
     safeGuard = new SafeGuard(manager);
     assertEq(safeGuard.manager(), manager);
